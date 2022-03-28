4a391a747846a34d7c53802d23719720ca5007a1
Nícalo Ribeiro <nicalo63@gmail.com>
2022-03-28 01:41:36 -0300

    Adding styles and fixing behaviors
    Co-authored-by: Joao Vitor <joaovitordmrosa@hotmail.com>

--- d-c_magic/src/App.js
+++ d-c_magic/src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [currentCardSet, setCurrentCardsSet] = useState([]);
   const [hasMoreToLoad, setHasMoreToLoad] = useState(false);
   const [cardName, setCardName] = useState('nissa');
-  const [sortBy, setSortBy] = useState('cmc');
+  const [sortBy, setSortBy] = useState('');
 
   const getDefaultData = async () => {
     setLoading(true);
@@ -18,11 +18,11 @@ function App() {
     setLoading(false)
   }
 
-  const handleSearchByName = async (name) => {
+  const handleSearchByName = async (name, page) => {
     setCurrentPage(1);
     setLoading(true);
     setHasMoreToLoad(true);
-    const response = await getAllCardsByName(name, currentPage);
+    const response = await getAllCardsByName(name, page);
     if (response.length > 0) {
       handleSort(response, sortBy);
     }
@@ -31,7 +31,7 @@ function App() {
 
   const handleLoadMore = async (name, page) => {
     setLoading(true);
-    const response = await getAllCardsByName(name, page);
+    const response = await getAllCardsByName(name, page + 1);
     if (response.length > 0) {
       setHasMoreToLoad(true);
       console.log('curr set', currentCardSet)
@@ -39,6 +39,7 @@ function App() {
       console.log('to sort', arrayToSort);
       handleSort(arrayToSort, sortBy);
       setLoading(false);
+      setCurrentPage((prev) => prev + 1);
       return;
     }
     setHasMoreToLoad(false);
@@ -50,33 +51,60 @@ function App() {
     const auxArr = [...arrayToSort];
     mergeSort(sortByType, auxArr, 0, arrayToSort.length - 1);
     setCurrentCardsSet(auxArr);
-    setCurrentPage((prev) => prev + 1)
   }
 
   useEffect(() => getDefaultData(), []);
 
   useEffect(() => console.log(currentCardSet), [currentCardSet]);
 
+  // useEffect(() => {
+  //   handleSort
+  // }, [sortBy])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {
-          hasMoreToLoad &&
-          <button onClick={() => handleLoadMore(cardName, currentPage)} type="button">Carregar mais</button>
+      <div className='searchBar'>
+        <label for="cardName">Buscar carta</label>
+        <input type="text" id="cardName" onChange={(e) => setCardName(e.target.value)} />
+        <button onClick={() => {
+          handleSearchByName(cardName, 1);
+        }} type="button" >Buscar</button>
+      </div>
+      {
+          currentCardSet.length > 0 &&
+          <div className='orderByDiv'>
+            <label for="orderBy">Ordernar por</label>
+            <select name="select" id="orderBy" onChange={(e) => {
+              setSortBy(e.target.value)
+              handleSort(currentCardSet, e.target.value)
+            }}>
+              <option disabled selected value> -- Selecione uma opção -- </option>
+              <option value="cmc">CMC (Custo de Mana Convertido)</option>
+              <option value="name">Nome</option>
+              <option value="setName">Coleção</option>
+            </select>
+          </div>
         }
-      </header>
+      <div className='mainContainer'>
+        <div style={{ display: 'flex', maxWidth: '1120px', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center' }}>
+          {currentCardSet.map((card) =>
+            <div className='cardBox' key={card.id}>
+              <div><img src={card.imageUrl} width="180" height="auto" /></div>
+              <div>
+                <p>{card.name}</p>
+                <p>CMC: {card.cmc}</p>
+                <p>Edição: {card.setName}</p>
+              </div>
+            </div>
+          )}
+
+        </div>
+        {
+          (hasMoreToLoad && currentCardSet.length > 0) &&
+          <button className="loadMore" onClick={() => handleLoadMore(cardName, currentPage)} type="button">Carregar mais</button>
+        }
+      </div>
+
     </div>
   );
 }
